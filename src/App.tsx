6231c8770869a4f8c5985dcc2f97c7c2184cb6a9
Başak Dönder <basakdonder01@gmail.com>
2022-06-02 12:03:16 +0300

    adding map and marker

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      coding test
-    </div>
-  );
+export default function App() {
+  const [coords, setCoords] = useState({
+    lat: 0,
+    lng: 0,
+  });
+
+  useLoadScript({
+    googleMapsApiKey: "",
+  });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCoords({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  function Map() {
+    return (
+      <GoogleMap
+        zoom={15}
+        center={{ lat: coords.lat, lng: coords.lng }}
+        mapContainerClassName="map-container"
+      >
+        <Marker
+          title="You're here!"
+          position={{ lat: coords.lat, lng: coords.lng }}
+        />
+      </GoogleMap>
+    );
+  }
+
+  return <Map />;
 }
-
-export default App;
